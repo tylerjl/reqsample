cae6e4eda3631bf384c3669e65c2fc35b5f33b72
Tyler Langlois <tjl@byu.net>
2017-09-07 01:26:57 -0400

    response code sampling

--- lib/reqsample/country.rb
+++ lib/reqsample/country.rb
@@ -3,10 +3,35 @@ require 'ipaddr'
 
 module ReqSample
   class Countries
-    attr_accessor :networks, :connectivity
+    attr_accessor :codes, :connectivity, :networks
+
+    # These probabilities are purely random guesses
+    RESPONSE_CODES = {
+      '200' => 100,
+      '204' => 1,
+      '301' => 5,
+      '302' => 10,
+      '304' => 30,
+      '400' => 3,
+      '401' => 2,
+      '403' => 6,
+      '404' => 13,
+      '429' => 3,
+      '500' => 2,
+      '502' => 7,
+      '503' => 3,
+      '504' => 3
+    }.freeze
 
     def initialize
       vendor = File.expand_path('../../../vendor', __FILE__)
+
+      @codes = RESPONSE_CODES.map do |code, weight|
+        [
+          code,
+          (Float weight) / RESPONSE_CODES.values.reduce(:+)
+        ]
+      end.to_h
 
       populations = JSON.parse(
         File.read(File.join(vendor, 'country_connectivity.json'))
@@ -26,7 +51,18 @@ module ReqSample
 
     def sample
       country = sample_country
-      { country => sample_address(country) }
+      {
+        country => {
+          :address => sample_address(country),
+          :code => sample_code
+        }
+      }
+    end
+
+    def sample_code
+      codes.max_by do |_, weight|
+        rand**(1.0 / weight)
+      end.first
     end
 
     def sample_country
