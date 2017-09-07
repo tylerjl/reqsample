bf3688d998cf6159de8f61e6f773b9ff8b5c95b7
Tyler Langlois <tjl@byu.net>
2017-09-07 00:50:49 -0400

    join together country and network sampling

--- lib/reqsample/country.rb
+++ lib/reqsample/country.rb
@@ -3,19 +3,42 @@ require 'ipaddr'
 
 module ReqSample
   class Countries
-    attr_accessor :countries
+    attr_accessor :networks, :connectivity
 
     def initialize
       vendor = File.expand_path('../../../vendor', __FILE__)
-      @countries = JSON.parse(
+
+      populations = JSON.parse(
+        File.read(File.join(vendor, 'country_connectivity.json'))
+      )
+      total_population = populations.values.reduce(:+)
+      @connectivity = populations.map do |country, population|
+        [
+          country,
+          (Float population) / total_population
+        ]
+      end.to_h
+
+      @networks = JSON.parse(
         File.read(File.join(vendor, 'country_networks.json'))
       )
     end
 
-    def sample(country = nil)
-      country ||= countries.keys.sample
+    def sample
+      country = sample_country
+      { country => sample_address(country) }
+    end
 
-      head, tail = countries[country].sample
+    def sample_country
+      connectivity.max_by do |_, weight|
+        rand**(1.0 / weight)
+      end.first
+    end
+
+    def sample_address(country = nil)
+      country ||= networks.keys.sample
+
+      head, tail = networks[country].sample
       IPAddr.new(
         rand(IPAddr.new(head).to_i..IPAddr.new(tail).to_i),
         Socket::AF_INET
